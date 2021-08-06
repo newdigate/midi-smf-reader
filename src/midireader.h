05938fc952e9a2ab1b6499e522808f823cfa4057
Newdigate <nicnewdigate@gmail.com>
2021-08-06 12:46:49 +0100

    add get_microseconds_per_tick

--- src/midireader.h
+++ src/midireader.h
@@ -45,6 +45,12 @@ public:
         return _numTracks;
     }
 
+    unsigned int get_microseconds_per_tick() {
+        double micros_per_beat = 60000000.0 / _currentBPM;
+        unsigned int micros_per_tick = micros_per_beat / 480;
+        return micros_per_tick;
+    }
+
 private:
     bool _initialized;
     File _midifile;
